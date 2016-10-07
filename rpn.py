9ffd6e664f894d156973e386b0665a162ffc1bdc
Eric Bogard <ebogard@umich.edu>
2016-10-07 14:56:40 -0400

    Hi

--- rpn.py
+++ rpn.py
@@ -1,20 +1,32 @@
 #!/usr/bin/env python3
 
+def add(a, b):
+	return a + b
+def subtract(a, b):
+	return a - b
+def multiply(a, b):
+	return a * b
+def divide(a, b):
+	return a / b
+
+operators = {
+	'+': add,
+	'-': subtract,
+	'*': multiply,
+	'/': divide
+}
+
 def calculate(myarg1):
 	stack = list()
 	for token in myarg1.split():
-		if token == '+':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg1 + arg2
-			stack.append(result)
-		elif token == '-':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg2 - arg1
-			stack.append(result)
-		else:
+		try:
 			stack.append(int(token))
+		except ValueError:
+			arg2 = stack.pop()
+			art1 = stack.pop()
+			function = operators[token]
+			result = function(arg1, arg2)
+			stack.append(result)
 		print(stack)
 	if len(stack) != 1:
 		raise TypeError
