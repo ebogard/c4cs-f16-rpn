e43bc82fec703b40459d139069afa0d9e4227c00
Eric Bogard <ebogard@umich.edu>
2016-10-13 11:54:53 -0400

    Add exponent test case

--- test_rpn.py
+++ test_rpn.py
@@ -7,6 +7,9 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate("1 1 +")
 		self.assertEqual(2, result)
+	def test_exponent(self)
+		result = rpn.calculate("2 2 ^")
+		self.assertEqual(4, result)
 	def test_toomanythings(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
